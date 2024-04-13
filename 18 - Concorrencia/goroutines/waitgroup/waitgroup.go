da9cdd977114e767fe34b1e0d4e7f3c5228a4428
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2024-04-12 21:49:38 -0300

    feat: waitgroup

---
+++ 18 - Concorrencia/goroutines/waitgroup/waitgroup.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var waitGroup sync.WaitGroup
+
+	waitGroup.Add(4)
+
+	go func() {
+		escrever("Ola mundo!")
+		waitGroup.Done()
+	}()
+
+	go func() {
+		escrever("Programando no go :D")
+	}()
+
+	go func() {
+		escrever("Muito fix!")
+		waitGroup.Done()
+	}()
+
+	go func() {
+		escrever("Programando na Europa")
+	}()
+
+	waitGroup.Wait()
+}
+
+func escrever(texto string) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(texto)
+		time.Sleep(time.Second)
+	}
+}
