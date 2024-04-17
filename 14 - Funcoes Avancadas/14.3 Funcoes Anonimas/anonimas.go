95f298ba7d040977e3a3b97d81a38e9fb18273a0
Daniel Fonseca <daniel.fonseca@dgtallab.com.br>
2024-04-17 01:36:23 +0100

    feat: add function anonymous

---
+++ 14 - Funcoes Avancadas/14.3 Funcoes Anonimas/anonimas.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	retorno := func(texto string) string {
+		return fmt.Sprintf("Recebido -> %s", texto)
+	}("Passando um parametro")
+
+	fmt.Println(retorno)
+}
