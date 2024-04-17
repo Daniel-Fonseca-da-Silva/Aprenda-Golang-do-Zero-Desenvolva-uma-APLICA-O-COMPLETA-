4869e3caabea42dc6853c7b84dd70bb96ec2b4df
Daniel Fonseca <daniel.fonseca@dgtallab.com.br>
2024-04-17 11:05:58 +0100

    feat: add init function

---
+++ 14 - Funcoes Avancadas/14.9 Init/init.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var n int
+
+// func init() {
+// 	fmt.Println("Executando a funcao init!")
+// 	n = 10
+// }
+
+func main() {
+	fmt.Println("Funcao main sendo executada!")
+	fmt.Println(n)
+}
