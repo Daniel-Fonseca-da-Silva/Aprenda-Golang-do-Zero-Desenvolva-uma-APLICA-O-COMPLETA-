6f2b1ac72359b2529b57e34871d0e88b91505ec3
Daniel Fonseca <daniel.fonseca@dgtallab.com.br>
2024-04-17 12:22:37 +0100

    feat: add function with sub test

---
+++ 19 - Testes Automatizados/2 - teste Avancado/formas/formas_test.go
@@ -0,0 +1,27 @@
+package formas
+
+import (
+	"math"
+	"testing"
+)
+
+func TestArea(t *testing.T) {
+	t.Run("Retangulo", func(t *testing.T) {
+		ret := Retangulo{10, 12}
+		areaEsperada := float64(120)
+		areaRecebida := ret.Area()
+
+		if areaEsperada != areaRecebida {
+			t.Fatalf("A area recebida %f é diferente da esperada %f", areaRecebida, areaEsperada)
+		}
+	})
+	t.Run("Ciruclo", func(t *testing.T) {
+		circ := Circulo{10}
+		areaEsperada := float64(math.Phi * 100)
+		areaRecebida := circ.Area()
+
+		if areaEsperada != areaRecebida {
+			t.Fatalf("A area recebida %f é diferente da esperada %f", areaRecebida, areaEsperada)
+		}
+	})
+}
---
+++ 19 - Testes Automatizados/2 - teste Avancado/go.mod
@@ -0,0 +1,3 @@
+module treino.com.pt
+
+go 1.22.2
