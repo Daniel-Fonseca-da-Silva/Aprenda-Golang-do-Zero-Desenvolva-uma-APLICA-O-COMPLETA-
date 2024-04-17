e2088617fe892eab8d2d6601cb28b5b9107abfad
Daniel Fonseca <daniel.fonseca@dgtallab.com.br>
2024-04-17 12:48:19 +0100

    feat: add application by terminal two

--- 17 - Aplicacao Linha de Comando/app/app.go
+++ 17 - Aplicacao Linha de Comando/app/app.go
@@ -1,11 +1,45 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicacao de linha de comando"
 	app.Usage = "Busca ips e nomes de servidor na internet"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca IPS de enderecos na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "dafon.com.br",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
+
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+
 }
